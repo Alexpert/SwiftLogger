4c7199a8c4818631a4f19f446afd5f5adbdd830f
Alexandre SALMON <salmon.alexandre10@gmail.com>
2020-01-28 10:51:27 +0100

    allez....

--- Sources/SwiftLogger/SwiftLogger.swift
+++ Sources/SwiftLogger/SwiftLogger.swift
@@ -1,36 +1,36 @@
 import Foundation
 
-enum LogType {
+public enum LogType {
     case ERROR
     case DEBUG
     case INFO
 }
 
-class SwiftLogger {
+public class SwiftLogger {
     var enabledLogTypes: [LogType]
     var path: String
     var domain: String
 
-    init(path: String, domain: String, logTypes: [LogType] = []) {
+    public init(path: String, domain: String, logTypes: [LogType] = []) {
         self.path = path
         self.domain = domain
         self.enabledLogTypes = logTypes
     }
 
-    func enableLogType(type: LogType) {
+    public func enableLogType(type: LogType) {
         if !enabledLogTypes.contains(type) {
             enabledLogTypes.append(type)
         }
     }
 
-    func disableLogType(type: LogType) {
+    public func disableLogType(type: LogType) {
         if enabledLogTypes.contains(type) {
             enabledLogTypes = enabledLogTypes.filter{ $0 != type}
         }
         
     }
 
-    func log(type: LogType, header: String, content: String) {
+    public func log(type: LogType, header: String, content: String) {
         if (enabledLogTypes.contains(type)) {
             let log = "\n[\(type)] \(domain.uppercased()): \(Date()) \n \t \(header): \(content)"
             let content = try! String(contentsOf: URL(fileURLWithPath: path), encoding: .utf8)
